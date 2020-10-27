5008c659a6f499fa3e0f29796bd0ef097e36e600
Teo2002 <71121136+Teo2002@users.noreply.github.com>
2020-10-27 10:05:10 +0100

    Ez a feladat is készen van

--- 008/main.py
+++ 008/main.py
@@ -1,9 +1,7 @@
-x=0.0
-y=0.0
-
 def Tavolsag(x,y):
     print(round(((0 - x) ** 2 + (0 - y) ** 2) ** 0.5, 2))
-    
+x=0.0
+y=0.0
 while True:
     irany = str(input())
     if irany == "stop": 
@@ -16,8 +14,7 @@ while True:
     if irany == "right": 
         x = x + egyseg
     if irany == "left": 
-        x = y - egyseg
-        
+        x = y - egyseg 
 print(round(x,2))
 print(round(y,2))
 Tavolsag(x,y)
